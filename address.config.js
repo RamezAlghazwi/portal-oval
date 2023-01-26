4624b6371de1ab1fc56a10bcdcbfd36820ddb16a
Simon Pokorny <simon.pokorny@outlook.com>
2023-01-26 18:53:17 +0100

    removed other assets from platform

--- address.config.js
+++ address.config.js
@@ -1,46 +1,16 @@
 module.exports = {
   whitelists: {
-    'publicKey.owner': [],
+    'publicKey.owner': ['0x2ee3c4F19f90237B7C45cfAD6B5dC4b5840563Ec'],
     dataToken: []
   },
   featured: [
     {
-      title: 'Industry',
-      assets: [
-        'did:op:a43270C235C8C2894feDFd5ec6eEa44764A95D3e',
-        'did:op:57F7F6e1a8a5D37dd028Abdd6f6EA213cB156f5f',
-        'did:op:3D3ABebA09077f354cf13D232bdbF1F1e97ca351',
-        'did:op:1075369913a092028a4C8fd18ccAdD85aC800144',
-        'did:op:3a0b09246457571009899C35B292057cA42DB773',
-        'did:op:0E8cEa497d897f8D18283B86b20eD6416281dC73'
-      ]
+      title: 'Tracks & Szenarios',
+      assets: ['did:op:5E2cd5bCC8ab1A6227b54626ecc9481cE18ACBF9']
     },
     {
-      title: 'Mobility',
-      assets: [
-        'did:op:a905d250B00b4CBd483225Bc3cF9E3cf8b02ac39',
-        'did:op:d51494c94d3aE025794034431f053e3fb6a26815',
-        'did:op:78f831361E1c850a16667346068f5fcAFE83F1F5',
-        'did:op:e69b8Ac90c25b099dC77b580B4d2019102fFCD3E',
-        'did:op:638B1671A45aF83E1ED0b394FC04d1A0307cD58d',
-        'did:op:3D1d22d5207bB1F6F8d8889263838C17A00d1d6F'
-      ]
-    },
-    {
-      title: 'Science',
-      assets: [
-        'did:op:1ea7A7F278669CfF1D0dD6CA608dd763fCcf8EE6',
-        'did:op:5A3E99a1d126F2546589bCF5f6cB541ab87Aad83',
-        'did:op:F4910E1B433Ad21140683c04D25309f08d6E1946',
-        'did:op:0a7cEc708Dd76f82fE2a510135Ab1C3BAaEC1E88'
-      ]
-    },
-    {
-      title: 'Health, Finance, Business Analytics, Energy and more',
-      assets: [
-        'did:op:048BdCeba7f43620302Cd7e593451e40f9e194F5',
-        'did:op:700b5C28396B4ceA3450A8DFA6297DF7c40C240f'
-      ]
+      title: 'Simulators',
+      assets: ['did:op:76b92C812Ac384425865329b1162fea6Af9246D1']
     }
   ]
 }
--- run.bash
+++ run.bash
@@ -1,2 +1,2 @@
-#docker build . -t portal-oval
+docker build . -t portal-oval
 docker run -p 5000:5000 portal-oval:latest
